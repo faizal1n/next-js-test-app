e95afc8dbdb7099ad2b0adc6699e1764726bacf3
Faizal Eko <faizal.eko@gmail.com>
2023-05-09 21:53:45 +0700

    login page integration

---
+++ pages/login.js
@@ -0,0 +1,90 @@
+import Link from "next/link";
+import { useRouter } from 'next/router';
+
+export default function Login() {
+  const router = useRouter();
+
+  async function onSubmitHandler(event) {
+    event.preventDefault();
+
+    const data = {
+      email: event.target.email.value,
+      password: event.target.password.value,
+    };
+
+    const JSONdata = JSON.stringify(data);
+
+    const endpoint = '/api/auth';
+
+    try {
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSONdata,
+      };
+
+      const response = await fetch(endpoint, options);
+
+      const result = await response.json();
+
+      console.log(result);
+
+      router.push('/')
+
+      return false;
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  return (
+    <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden">
+      <div className="w-full p-6 bg-white rounded-md shadow-md lg:max-w-xl">
+        <h1 className="text-3xl font-bold text-center text-gray-700">Logo</h1>
+        <form className="mt-6" onSubmit={onSubmitHandler}>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label
+              htmlFor="password"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <Link
+            href="/forget"
+            className="text-xs text-blue-600 hover:underline"
+          >
+            Forget Password?
+          </Link>
+          <div className="mt-2">
+            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
